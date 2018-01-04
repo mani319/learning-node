fcd729490fd2f385496d02b5217ecfe57edc2120
Manikanta T <manikanta.t@myntra.com>
2018-01-04 22:41:43 +0530

    Using morgan to observe the traffic Understanding routes basics Using params of routes(request)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var url = require('url');
+var morgan = require('morgan')
+
+app.use(morgan('short'))
 
 app.get('/', function(req, res) {
-    var queryObject = url.parse(req.url, true).query;
-    var name = queryObject.name || "Trump";
+    res.send("This is default page")
+});
+
+app.get('/name/:statement', function(req, res) {
+    var name = req.params.statement || "Trump";
 
     res.send("This is my first node app - " + name);
 });
