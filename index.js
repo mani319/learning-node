6a27995e0bb7faded67043d1c38080c104bd5d54
Manikanta T <manikanta.t@myntra.com>
2018-01-04 20:55:43 +0530

    Querying the name
    Modified index.js such that it can take url and query "name" using web server
    
    localhost:8080?name="My Name" shows name on screen
    Any other url is defaulted to show the name Trump

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var http = require('http');
+var url = require('url');
+
 var httpServer = http.createServer(function(req, res) {
-    res.end("This is my first node app");
+    var queryObject = url.parse(req.url, true).query;
+    var name = queryObject.name || "Trump";
+
+    res.end("This is my first node app - " + name);
 });
 
 httpServer.listen(8080, function() {
